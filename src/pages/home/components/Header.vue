e431de9ead84baa654cdcf28f36cde51192ea2ee
alin <1353104310@qq.com>
2019-03-01 00:47:20 +0800

    初始化项目

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <div class="header-left">ddd</div>
+    <div class="header-input"></div>
+    <div class="header-right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    height: 0.86rem
+</style>
