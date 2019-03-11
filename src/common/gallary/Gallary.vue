983648a88153ba3195b6830d47bbe1757b497882
Alin <alin@dreampuppet.com>
2019-03-11 20:29:36 +0800

    优化detail

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="gallary" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <swiper-slide v-for="(item, index) of imgs" :key="index">
+          <img class="gallary-img" :src="item">
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallary',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'fraction'
+        },
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .gallary >>> .swiper-container
+    overflow: inherit
+  .gallary
+    position absolute
+    top 0
+    left 0
+    right 0
+    bottom 0
+    background #000
+    display flex
+    align-items center
+    .wrapper
+      width 100%
+      .gallary-img
+        width: 100%
+      .swiper-pagination
+        color: #fff
+        bottom: -1rem
+</style>
--- src/pages/detail/component/Banner.vue
+++ src/pages/detail/component/Banner.vue
@@ -1,23 +1,52 @@
 <template>
-  <div class="banner">
-    <img class="banner-img" :src="bannerImg" :alt="sightName">
-    <div class="banner-info">
-      <div class="title">{{sightName}}</div>
-      <div class="number-warpper">
-        <span class="iconfont">&#xe61b;</span>
-        <span class="number">{{bannerImgs.length}}</span>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" :src="bannerImg" :alt="sightName">
+      <div class="banner-info">
+        <div class="title">{{sightName}}</div>
+        <div class="number-warpper">
+          <span class="iconfont">&#xe61b;</span>
+          <span class="number">{{bannerImgs.length}}</span>
+        </div>
       </div>
     </div>
+    <fade-animation>
+      <common-gallary
+        :imgs="bannerImgs"
+        v-show="showGallary"
+        @close="handleGallaryClose">
+      </common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
+import CommonGallary from '@/common/gallary/Gallary'
+import FadeAnimation from '@/common/fade/FadeAnimation'
+
 export default {
   name: 'DetailBanner',
   props: {
     sightName: String,
     bannerImg: String,
     bannerImgs: Array
+  },
+  data () {
+    return {
+      showGallary: false
+    }
+  },
+  components: {
+    CommonGallary,
+    FadeAnimation
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
   }
 }
 </script>
